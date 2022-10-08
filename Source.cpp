2a0ca22be0c87145a0a56695832ba1f80b2f0b92
LAZENGANNN <113790875+LAZENGANNN@users.noreply.github.com>
2022-10-08 18:33:15 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,95 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	
+	//#1
+	/*int hp;
+	bool res;
+	cin >> hp;
+	if (hp > 0) {
+		res = 1;
+	}
+	else {
+		res = 0;
+	}
+	cout << boolalpha << res;*/
+
+
+	//#2
+	/*int t;
+	cin >> t;
+	if (t > 20){
+		cout << "on";
+	}
+	else {
+		cout << "off";
+	}*/
+
+	//#3
+	/*int v;
+	int t;
+	int Tpult;
+	cin >> v >> Tpult >> t;
+	if (v < 80 && Tpult < t)
+	{
+		cout << "on";
+	}
+	else
+	{
+		cout << "off";
+	}*/
+
+	//#4
+	/*int x;
+	int y;
+	cin >> x;
+	if (x>0) 
+	{
+		y = sqrt(x-2);
+	}
+	else if (x == 0)
+	{
+		y = 0;
+	}
+	else if (x < 0)
+	{
+		y = abs(x);
+	}
+	cout << y;*/
+
+	//#5
+	/*int p;
+	cin >> p;
+	switch(p)
+	{
+	case 1: cout << "большой палец"; break;
+	case 2: cout << "указательный"; break;
+	case 3: cout << "средний"; break;
+	case 4: cout << "безымянный"; break;
+	case 5: cout << "мизинец"; break;
+	default: cout << "ошибка"; break;
+	}
+	return 0;*/
+
+	//#6
+	int troom;
+	int tstreet;
+	cin >> troom >> tstreet;
+	if (troom > 20 && tstreet > troom )
+	{
+		cout << "ac on";
+	}
+	else if (troom <= 20)
+	{
+		cout << "ac off";
+	}
+	else if (troom > 20 && tstreet < troom)
+	{
+		cout << "open window";
+	}
+
+
+
+}
